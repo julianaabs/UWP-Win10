fae8ec8599127799e8c4e246bd0f169aa8713522
Juliana <jubsbarbosa0@gmail.com>
2017-10-18 11:29:28 -0300

    xaml

--- WhatIsXAML/MainPage.xaml.cs
+++ WhatIsXAML/MainPage.xaml.cs
@@ -31,5 +31,22 @@ namespace WhatIsXAML
         {
             MessageTextBlock.Text = "What is XAML?";
         }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            /*Button myButton = new Button();
+            myButton.Name = "ClickMeButton";
+            myButton.Content = "Click Me";
+            myButton.Width = 200;
+            myButton.Height = 100;
+            myButton.Margin = new Thickness(20, 20, 0, 0);
+            myButton.HorizontalAlignment = HorizontalAlignment.Left;
+            myButton.VerticalAlignment = VerticalAlignment.Top;
+
+            myButton.Background = new SolidColorBrush(Windows.UI.Colors.Red);
+            myButton.Click += ClickMeButton_Click;
+
+            LayoutGrid.Children.Add(myButton);*/
+        }
     }
 }
